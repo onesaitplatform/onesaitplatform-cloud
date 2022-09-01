05883304c2047af9a9e6b09aab28af83b686a4e5
Miguel Morán Cassy <miguelmorancassy@hal9000.local>
2022-09-01 17:33:13 +0200

    Permissions fixed

--- sources/modules/api-manager/docker/docker-entrypoint.sh
+++ sources/modules/api-manager/docker/docker-entrypoint.sh
@@ -5,8 +5,7 @@ echo "Finding Plugins"
 if [ -z "$PLUGIN_URI" ]
 then
       echo "No plugins specified"
-else
-     
+else     
       echo "Downloading Plugins..."
 	  IFS=';' read -ra plugins <<< "$PLUGIN_URI"
 	  for plugin in "${plugins[@]}"
@@ -14,8 +13,7 @@ else
 		  echo "Plugin found on '$plugin'"
 	      wget -P /application/BOOT-INF/lib/ $plugin
 	    	
-	  done
-      
+	  done      
 fi		
 
 java $JAVA_OPTS -Dspring.application.json=$ONESAIT_PROPERTIES -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=docker org.springframework.boot.loader.JarLauncher
